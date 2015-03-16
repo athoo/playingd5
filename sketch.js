5f599968918f6033c3a96376b71982c575dfcfc8
athoo <athoo@sina.com>
2015-03-17 05:43:45 +0800

    apple

--- sketch.js
+++ sketch.js
@@ -1,23 +1,30 @@
-var img;
-var canvas;
-
+var img;  // Declare variable 'img'.
+var i=0;
 function setup() {
-
-  img = createImg("http://th07.deviantart.net/fs70/PRE/i/2011/260/3/5/dash_hooray_by_rainbowcrab-d49xk0d.png");
-  canvas = createCanvas(400, 400);
-
-  img.position(190, 50);
-  img.size(200, AUTO); // Use AUTO to have the height or width auto scale. 
-  canvas.position(300, 50);
+  createCanvas(screen.width, screen.height);
+  img = loadImage("assets/cat.jpg");  // Load the image
 }
 
+
 function draw() {
-  noStroke();
-  background(220, 180, 200);
-  fill(180, 200, 40);
-  strokeWeight(6);
-  stroke(180, 100, 240);
-  for (var i=0; i<width; i+=15) {
-    line(i, 0, i, height);
+  // Displays the image at its actual size at point (0,0)
+  image(img, 0, 0);
+  // Displays the image at point (0, height/2) at half size
+    // image(img, 0, height/2, img.width/2, i/2);
+    color(red);
+  // drawRect();
+
+  copy(img,10,10,30,30,700,300,30,30);
+}
+
+function drawRect(){
+
+    fill(0);
+  if(mousePressed==true){
+    mouseReleased(){
+      ellipse(mouseX,mouseY,80,80);
+      return false;
+    }
   }
+
 }
